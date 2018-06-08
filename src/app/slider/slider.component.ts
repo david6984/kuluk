a9235f9c26e09d481e0355be9eb2fdf5dd2475ba
David Villalobos <davidevm@Davids-MacBook-Pro.local>
2018-06-08 16:30:46 -0600

    hola

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -11,13 +11,11 @@ export class SliderComponent implements OnInit {
 
   constructor() {
   	this.items=[
-      {name:'http://res.cloudinary.com/kuluk/image/upload/v1518043522/27067332_1630745313671296_8162047250216444593_n.jpg'},
-      {name:'http://res.cloudinary.com/kuluk/image/upload/v1519266130/KULUK-PCover02.png'},
-  		{name:'http://res.cloudinary.com/kuluk/image/upload/v1520619509/28701033_1670724599673367_2486273112074060735_o.jpg'},
-  		{name:'http://res.cloudinary.com/kuluk/image/upload/v1520619504/21077731_1488414601237702_4096480052720430901_n.jpg'},
-  		{name:'http://res.cloudinary.com/kuluk/image/upload/v1519266841/Kuluk_foto_producto.jpg'},
-  		{name:'http://res.cloudinary.com/kuluk/image/upload/v1519266936/Kuluk_GIFv3a.gif'},
-  		{name:'http://res.cloudinary.com/kuluk/image/upload/v1519266135/KULUK-PCover01.png'},
+      {name:'https://res.cloudinary.com/kuluk/image/upload/v1518043522/27067332_1630745313671296_8162047250216444593_n.jpg'},
+      {name:'https://res.cloudinary.com/kuluk/image/upload/v1519266130/KULUK-PCover02.png'},
+  		{name:'https://res.cloudinary.com/kuluk/image/upload/v1520619504/21077731_1488414601237702_4096480052720430901_n.jpg'},
+  		{name:'https://res.cloudinary.com/kuluk/image/upload/v1519266135/KULUK-PCover01.png'},
+      {name:'https://res.cloudinary.com/kuluk/image/upload/v1518043522/26992494_1621888707890290_4134151992699333185_n.jpg'},
   	]; 
   }
 
