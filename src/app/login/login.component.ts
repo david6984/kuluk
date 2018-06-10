08eab28e0174ef03641f23628d9787700f0f9824
David Villalobos <davidevm@Davids-MacBook-Pro.local>
2018-06-09 21:45:50 -0600

    usuariologin

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -31,6 +31,7 @@ export class LoginComponent implements OnInit {
         			console.log('el usuario logeo correctamente');
         		}else{
         			console.log('usuario invalido');
+        			this.loading=false;
         		}
         	}
         },(error)=>{
