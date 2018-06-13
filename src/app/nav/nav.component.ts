afa0792c5fb2b5325cb3baca71333549490b6b3d
David Villalobos <davidevm@Davids-MacBook-Pro.local>
2018-06-13 15:29:17 -0600

    userlogin

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -18,6 +18,7 @@ export class NavComponent implements OnInit {
     this.router.events.subscribe((event)=>{
       if(event instanceof Navigator){
         this.router.navigated=false;
+        this.router.navigate(['/nav']);
       }
     });
    }
