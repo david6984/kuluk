1ba96e2f8ae476db277890e01987ec3468d863f2
David Villalobos <davidevm@Davids-MacBook-Pro.local>
2018-06-13 22:34:08 -0600

    userlogin

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Output, EventEmitter, Input, OnChanges } from '@angu
 import { Injectable } from '@angular/core';
 import { RouterLinkActive, RouterLink, Router} from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { DatasharingService } from '../services/datasharing.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,22 +10,18 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-	public islogged:boolean=false;
+	public islogged:boolean;
 
-  constructor(private authService:AuthService,private router:Router ) {
+  constructor(private authService:AuthService,private router:Router,private dataSharing:DatasharingService) {
+        // Subscribe here, this will automatically update 
+        // "isUserLoggedIn" whenever a change to the subject is made.
+        this.dataSharing.isUserLoggedIn.subscribe( value => {
+            this.islogged = value;
+        });
   	this.ngOnInit();
    }
-   
-  ngOnInit() {
-  }
 
-  refrescar(){
-  	if(this.authService.islogged==true){
-      console.log('if del nav');
-  		this.islogged=true;
-  	}else{
-      this.islogged=false;
-    }
+  ngOnInit() {
   }
 
 }
