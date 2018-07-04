54b2eb4e9ad41e9261542a1609ad60b790f7f4b0
David Villalobos <davidevm@Davids-MacBook-Pro.local>
2018-07-04 14:21:06 -0600

    sliderServer

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -17,13 +17,13 @@ public slider:Slider = new Slider();
 public name:string='';
 
 constructor(private sliderService:SliderService) {
-  this.items=[
+ /* this.items=[
     {name:'https://res.cloudinary.com/kuluk/image/upload/v1518043522/27067332_1630745313671296_8162047250216444593_n.jpg'},
     {name:'https://res.cloudinary.com/kuluk/image/upload/v1519266130/KULUK-PCover02.png'},
     {name:'https://res.cloudinary.com/kuluk/image/upload/v1520619504/21077731_1488414601237702_4096480052720430901_n.jpg'},
     {name:'https://res.cloudinary.com/kuluk/image/upload/v1519266135/KULUK-PCover01.png'},
     {name:'https://res.cloudinary.com/kuluk/image/upload/v1518043522/26992494_1621888707890290_4134151992699333185_n.jpg'},
-  ]; 
+  ];*/ 
 }
 
 ngOnInit() {
